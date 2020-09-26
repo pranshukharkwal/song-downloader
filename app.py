aa314cd670f3212785e6c6a7e650af55f6ed2d30
Signior-X <b19188@students.iitmandi.ac.in>
2020-09-27 04:17:49 +0530

    Addded new api

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, redirect, url_for, session, request, logging , send_file
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging, send_file
 from flask_mysqldb import MySQL
 from functools import wraps
 import requests
@@ -16,19 +16,21 @@ from googleapiclient.discovery import build
 api_key = "AIzaSyASrsYEpNv9snbGjnilTRD4pWOBZXpOi1k"
 # End of the youtube apis
 
+
 def clear():
     print("Clear running")
     for i in os.listdir():
         if os.path.splitext(i)[1] == '.mp3' or os.path.splitext(i)[1] == '.mp4':
             os.remove(i)
 
+
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=clear,trigger="interval",hours = 1)
+scheduler.add_job(func=clear, trigger="interval", hours=1)
 atexit.register(lambda: scheduler.shutdown())
 scheduler.start()
 
 app = Flask(__name__)
-app.secret_key='secret123'
+app.secret_key = 'secret123'
 app.config['MYSQL_HOST'] = 'remotemysql.com'
 app.config['MYSQL_USER'] = 'JJFKCXD3CC'
 app.config['MYSQL_PASSWORD'] = 'us8bg5jdXp'
@@ -38,6 +40,7 @@ mysql = MySQL(app)
 
 data = dict()
 
+
 def download_by_url(yturl):
 
     data = {}
@@ -45,10 +48,11 @@ def download_by_url(yturl):
     try:
         yt = YouTube(yturl)
         cur = mysql.connection.cursor()
-        if cur.execute("select * from songs where song = %s" , (yt.title,)) > 0:
-            cur.execute("update songs set count = count + 1 where song = %s;" , (yt.title,))
+        if cur.execute("select * from songs where song = %s", (yt.title,)) > 0:
+            cur.execute(
+                "update songs set count = count + 1 where song = %s;", (yt.title,))
         else:
-            cur.execute("insert into songs(song) values(%s)",(yt.title,))
+            cur.execute("insert into songs(song) values(%s)", (yt.title,))
         mysql.connection.commit()
         cur.close()
     except:
@@ -63,7 +67,8 @@ def download_by_url(yturl):
         base, ext = os.path.splitext(out_file)
         new_file = base + '.mp3'
 
-        data['url'] = 'https://la-musica.herokuapp.com/'+video.default_filename[:-4] + '.mp3'
+        data['url'] = 'https://la-musica.herokuapp.com/' + \
+            video.default_filename[:-4] + '.mp3'
         data['vid_url'] = vid_url
         data['title'] = video.default_filename[:-4]
         data['image'] = yt.thumbnail_url
@@ -93,11 +98,11 @@ def downloader(query):
 
     res = req.execute()
 
-    y_items = res['items'] # This is a list of dictionaries
+    y_items = res['items']  # This is a list of dictionaries
 
     # Now try for all the results
     for row in y_items:
-    # print(res['items'][0]['id']['videoId']) # This gives the videoId
+        # print(res['items'][0]['id']['videoId']) # This gives the videoId
         sharedVideoUrl = 'https://youtu.be/' + row['id']['videoId']
         print("sharedVideoUrl ", sharedVideoUrl)
 
@@ -107,10 +112,11 @@ def downloader(query):
 
             # update database
             cur = mysql.connection.cursor()
-            if cur.execute("select * from songs where song = %s" , (yt.title,)) > 0:
-                cur.execute("update songs set count = count + 1 where song = %s;" , (yt.title,))
+            if cur.execute("select * from songs where song = %s", (yt.title,)) > 0:
+                cur.execute(
+                    "update songs set count = count + 1 where song = %s;", (yt.title,))
             else:
-                cur.execute("insert into songs(song) values(%s)",(yt.title,))
+                cur.execute("insert into songs(song) values(%s)", (yt.title,))
             mysql.connection.commit()
             cur.close()
 
@@ -152,31 +158,33 @@ def downloader(query):
     #         yt = YouTube(link)
 
 
-
-@app.route('/' , methods=['GET' , 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         query = request.form['query']
         data = downloader(query)
         # return 'checking'
-        return render_template('home.html' , data = data)
+        return render_template('home.html', data=data)
     else:
         return render_template('home.html')
 
+
 @app.route('/api/getdata', methods=['POST'])
 def rest_api():
-    data = request.get_json();
+    data = request.get_json()
     result = downloader(data['name'])
-    result['url']='https://la-musica.herokuapp.com/'+result['url']
-    #print(result)
-    return result  #Process Done
+    result['url'] = 'https://la-musica.herokuapp.com/'+result['url']
+    # print(result)
+    return result  # Process Done
+
 
 @app.route('/api/getytlink', methods=['POST'])
 def test_api2():
-    data = request.get_json();
+    data = request.get_json()
     result = download_by_url(data['yturl'])
-    #print(result)
-    return result # Take care of error also
+    # print(result)
+    return result  # Take care of error also
+
 
 @app.route('/api/getdownloads', methods=['GET'])
 def rest_api3():
@@ -186,20 +194,81 @@ def rest_api3():
     for x in cur.fetchall():
         songslst.append(x)
 
-    #print(songslst)
+    # print(songslst)
 
-    data = { "list": songslst }
-    #songslst is the list of song names in String
+    data = {"list": songslst}
+    # songslst is the list of song names in String
     return data
+
+
+'''
+API to get only the streaming link for youutbe
+Required json Data
+{
+	"name": "Name of the song"
+}
+
+Response
+{
+	"success": 1 or 0,
+	"url": "The video url",
+	"error_msg": "Only in case of succes not equal to 1"
+}
+'''
+
+
+@app.route('/api/getonlylink', methods=['POST'])
+def rest_api4():
+    data = request.get_json()
+    name = data['name']
+
+    data = {}
+    data['url'] = ''
+
+    try:
+
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        # print(type(youtube))
+
+        req = youtube.search().list(part="snippet", q=query, type="video")
+
+        # print(type(req))
+
+        res = req.execute()
+
+        y_items = res['items']  # This is a list of dictionaries
+
+        # Now try for all the results
+        for row in y_items:
+            # print(res['items'][0]['id']['videoId']) # This gives the videoId
+            sharedVideoUrl = 'https://youtu.be/' + row['id']['videoId']
+            print("sharedVideoUrl ", sharedVideoUrl)
+
+            yt = YouTube(sharedVideoUrl)
+
+            # check that it is a song
+            if yt.length <= 900:
+                vid_url = yt.streams.filter(only_audio=True)[0].url
+                data['url'] = vid_url
+                data['success'] = 1
+
+        data['success'] = 1
+    except Exception as e:
+        data['success'] = 0
+        data['error'] = str(e)
+
+    return data
+
 
 @app.route('/api/check', methods=['GET'])
 def restapi_chech():
     return 'API is Working!'
 
+
 @app.route('/<path>')
 def download_file(path):
-    return send_file(path , as_attachment = True)
+    return send_file(path, as_attachment=True)
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
